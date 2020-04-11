3f5d88b095c0b6fa6383d0529e60ea38c5ab5231
hamza BENKHAY <hamza@geocene.com>
2020-04-11 14:57:52 +0200

    yum -faefdsy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,4 +6,8 @@ yum -y install python36
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
 cd /home/ec2-user/
+pip-3.6 install virtualenv
+virtualenv environment
+source environment/bin/activate
 pip-3.6 install -r requirements.txt
+behave -f allure_behave.formatter:AllureFormatter -o ./allure-results
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 service httpd start
 cd /home/ec2-user/
-python3 build_reports.py
-ls
-behave -f allure_behave.formatter:AllureFormatter -o ./allure-results
