d0a08fb51800e207d8e080156ded7a3f01917d8c
hamza BENKHAY <hamza@geocene.com>
2020-04-11 14:29:00 +0200

    yum -faefdsy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 yum install -y httpd
+yum -y install docker
+service docker start
+yum install python36-setuptools
+easy_install-3.6 pip
+cd /home/ec2-user
+ls
+pip-3.6 install -r requirements.txt
+python3.6 build_reports.py
