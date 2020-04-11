d0e98c36ef5fbff51c33ffdc6f0162702ad1c8dc
hamza BENKHAY <hamza@geocene.com>
2020-04-11 12:29:00 +0200

    d

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,4 @@ yum install -y httpd
 yum -y install docker
 service docker start
 yum -y install python36
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py --user
 pip3 install -r ../requirements.txt
