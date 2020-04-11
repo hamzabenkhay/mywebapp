e86b2950751170fb87abb5939e607b400f9613f6
hamza BENKHAY <hamza@geocene.com>
2020-04-11 13:50:38 +0200

    yum -y

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,7 +4,8 @@ yum -y install docker
 service docker start
 yum install python36-setuptools
 easy_install-3.6 pip
-cd /home/ec2-user/
+cd /home/ec2-user/src
+ls
 pip-3.6 install -r requirements.txt
 ls
 python3.6 build_reports.py
